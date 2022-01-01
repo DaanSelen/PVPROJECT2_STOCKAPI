9b2cce954e7c8d158c8e4e7a53125d4768e4f388
Daan Selen <selendaan@hotmail.com>
2022-01-01 22:12:12 +0100

    Initial post

---
+++ database.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+var (
+	voorraad *sql.DB
+	err      error
+)
+
+func initDBConn(password string) {
+	_, err := sql.Open("mysql", "daan:"+password+"@tcp(192.168.178.23:3307)/voorraad")
+	if err != nil {
+		log.Println("Connection to Database Server failed: ", err)
+	}
+}
+
+func retrieveKruidenAmount() {
+	data, err := voorraad.Query("")
+	handleError(err)
+	defer data.Close()
+	for data.Next() {
+
+	}
+}
