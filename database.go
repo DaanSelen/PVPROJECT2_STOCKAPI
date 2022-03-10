9d1d430975a1d93375562a959cf6f6a22d5ecd7e
Daan Selen <selendaan@hotmail.com>
2022-03-10 13:30:39 +0100

    change 13 changed name application to logic, added test file for testing functions and added correcting function when answering no

--- database.go
+++ database.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -18,63 +17,6 @@ func initAndTestDBConn() {
 		handleError(err, "Initialising database connection")
 	} else {
 		testConnection()
-	}
-}
-
-func testConnection() {
-	answer := askDBQuestion()
-	log.Println(infoTag, "TESTING DATABASE CONNECTION")
-	_, err := voorraad.Query("SHOW Databases")
-	if err != nil {
-		log.Println(errorTag, "DATABASE CONNECTION FAILED. If you don't know what to do, contact an admin!")
-		closeApp()
-	} else {
-		log.Println(infoTag, "DATABASE SERVER AND DATABASE CONNECTION SUCCES, TESTING DATABASE TABLES.")
-		if answer == false {
-			if integrityCheck() {
-				createTableItems()
-			}
-		} else {
-			_ = integrityCheck()
-		}
-	}
-}
-
-func integrityCheck() bool {
-	if x := testTables(); x == len(cat) {
-		log.Println(infoTag, "DATABASE INTEGRITY VALIDATED")
-		return true
-	} else {
-		return integrityCheck() //true
-	}
-}
-
-func testTables() int {
-	var grade int = 0
-	for x := range cat {
-		_, err := voorraad.Query("SELECT * from " + cat[x])
-		if err != nil {
-			createMissingTable(cat[x])
-		} else {
-			grade++
-		}
-	}
-	return grade
-}
-
-func createMissingTable(keyword string) {
-	log.Println(infoTag, "CREATING TABLE:", keyword)
-	_, err := voorraad.Query("CREATE TABLE " + keyword + " (ID INT(20) NOT NULL, Naam VARCHAR(50) NOT NULL, Hoeveelheid INT(50) NOT NULL, Status VARCHAR(100) NOT NULL)")
-	handleError(err, "Creating "+keyword+" table")
-}
-
-func createTableItems() {
-	log.Println(infoTag, "INSERTING PRODUCTS")
-	for a, b := range productData {
-		for c := range b {
-			_, err := voorraad.Query(("INSERT INTO " + cat[a] + " (ID, Naam, Hoeveelheid, Status) VALUES (" + changeToString(retrieveMaxID(cat[a])+1) + ", '" + productData[a][c] + "', 0, 'N.v.t');"))
-			handleError(err, "creating table items")
-		}
 	}
 }
 
